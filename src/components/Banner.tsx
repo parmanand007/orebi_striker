ac8b84d308312ab43450940c95330421f1e75504
parmanand.prajapati <parmanand.prajapati@doctustech.com>
2024-06-11 23:32:36 +0530

    setup navbar

---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid possimus saepe perspiciatis quas quia autem. Nostrum modi consectetur ut quia eveniet, pariatur ab temporibus commodi laborum esse placeat ducimus iste, voluptate aliquid veniam non ratione assumenda nemo excepturi! Voluptas corporis natus hic nulla, eius veniam? Quod deserunt asperiores laborum ratione ea voluptate iusto, assumenda itaque adipisci ab corrupti qui voluptates provident laudantium vitae debitis quae praesentium sequi inventore sapiente. Velit error distinctio eligendi quos sapiente aliquid repellendus quibusdam sit quidem explicabo alias reiciendis molestias, itaque nobis veritatis fugiat! Labore cumque at minus, nemo animi dolorem accusantium velit ratione similique nesciunt.
+        </div>
+  )
+}
+
+export default Banner
