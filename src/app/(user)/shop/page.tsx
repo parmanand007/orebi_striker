ecd9dae2ae4abc253380de30c7698a67fffb5522
parmanand.prajapati <parmanand.prajapati@doctustech.com>
2024-07-01 23:54:45 +0530

    single prod slug page

--- src/app/(user)/shop/page.tsx
+++ src/app/(user)/shop/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+'use client'
+import Container from '@/components/Container'
+import React, { useState } from 'react'
+import { BsGridFill } from 'react-icons/bs'
 
 const ShopPage = () => {
+  const [showGrid,setShowGrid] = useState(true)
+  const [showList,setShowList] = useState(false)
   return (
-    <div>ShopPage</div>
+    <Container>
+      <div className='flex items-center justify-between pb-10'>
+        <h2 className='text-2xl text-primeColor font-bold'>
+          All Products
+        </h2>
+        <div>
+           <span className={`${showGrid ?'bg-primeColor text-white':'border-[1px] border-grey-300 text-[#737373]'} w-8 h-8 text-lg flex items-center justify-center cursor-pointer`}><BsGridFill /></span>
+        </div>
+      </div>
+    </Container>
   )
 }
 
-export default ShopPage
+export default ShopPage 
--- src/components/Onsale.tsx
+++ src/components/Onsale.tsx
@@ -15,7 +15,7 @@ const Onsale = ({products}:Props) => {
       <h3 className='text-xl font-semibold mb-6 underline underline-offset-4 decoration-[1px]'>Products on a sale</h3>
       <div className='flex flex-col justify-normal gap-2'>
         {products?.map((item:ProductProps)=>(
-          <Link href={`/product/${item.slug?.current}`} className='flex items-center gap-4 border-b-[1px] border-b-gray-300 py-2'>
+          <Link key ={item?._id} href={`/product/${item.slug?.current}`} className='flex items-center gap-4 border-b-[1px] border-b-gray-300 py-2'>
            
             <Image  src = {urlForImage(item?.image)} alt="product image"
             className='w-24 object-contain' width={200} height={200}/>
