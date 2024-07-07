105ecfac19091b4a096bea706854b5410c856dd3
parmanand.prajapati <parmanand.prajapati@doctustech.com>
2024-07-07 22:51:14 +0530

    create shop page  in list and grid view

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'next-sanity'
+import { createClient, groq } from 'next-sanity'
 import { apiVersion, dataset, projectId, useCdn, token } from '../env'
 
 export const client = createClient({
@@ -10,3 +10,10 @@ export const client = createClient({
   token
 })
 
+const productQuery = groq`*[_type == 'product']{
+    ...} | order(_createdAt desc)`;
+
+export const products = async() => {
+  const productData = await client.fetch(productQuery);
+  return productData;
+}
--- src/app/(user)/shop/page.tsx
+++ src/app/(user)/shop/page.tsx
@@ -1,21 +1,62 @@
 'use client'
 import Container from '@/components/Container'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsGridFill } from 'react-icons/bs'
+import { ImList } from 'react-icons/im'
+import { products } from '../../../../sanity/lib/client'
+import { ProductProps } from '../../../../type'
+import Product from '@/components/Product'
+import ListProduct from '@/components/ListProduct'
 
 const ShopPage = () => {
-  const [showGrid,setShowGrid] = useState(true)
+  const [showGrid,setShowGrid] = useState(false)
   const [showList,setShowList] = useState(false)
+  const [productData,setProductData] = useState([]);
+  useEffect(()=>{
+    const fetchData = async()=> {
+      try{
+          const data = await products();
+          setProductData(data);
+      }
+      catch(error){
+       console.log("Error fetching product Data:",error)
+      }
+    }
+    fetchData();
+  },[]);
   return (
     <Container>
       <div className='flex items-center justify-between pb-10'>
         <h2 className='text-2xl text-primeColor font-bold'>
           All Products
         </h2>
-        <div>
-           <span className={`${showGrid ?'bg-primeColor text-white':'border-[1px] border-grey-300 text-[#737373]'} w-8 h-8 text-lg flex items-center justify-center cursor-pointer`}><BsGridFill /></span>
+        <div className='flex items-center gap-2'>
+           <span 
+           onClick={()=>{
+            setShowGrid(true)
+            setShowList(false)
+           }}
+           className={`${showGrid ?'bg-primeColor text-white':'border-[1px] border-grey-300 text-[#737373]'} w-8 h-8 text-lg flex items-center justify-center cursor-pointer`}><BsGridFill />
+           
+           </span>
+           <span 
+           onClick={()=>{
+            setShowGrid(false)
+            setShowList(true)
+           }}
+           className={`${showList ?'bg-primeColor text-white':'border-[1px] border-grey-300 text-[#737373]'} w-8 h-8 text-lg flex items-center justify-center cursor-pointer`}><ImList />
+           
+           </span>
         </div>
       </div>
+      {showGrid ? (<div className='w-full grid grid-cols-1 md:grid-cols-2 xl:grid-col-4 gap-5'>
+        {productData?.map((item:ProductProps)=>(
+          <Product key = {item?._id} product= {item}/>
+        ))}
+      </div>) : (<div className='w-full grid grid-cols-1 gap-5'>
+      {productData?.map((item:ProductProps)=>(
+          <ListProduct key = {item?._id} product= {item}/>
+        ))}</div>)}
     </Container>
   )
 }
