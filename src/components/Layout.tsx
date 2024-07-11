a85d677c1cac4739c21a1a438709f61d8c49138d
parmanand.prajapati <parmanand.prajapati@doctustech.com>
2024-07-11 22:23:02 +0530

    hold state even on reload

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,16 @@
 'use client'
-import { store } from '@/redux/store'
+import { store,persistor } from '@/redux/store'
 import React from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const Layout = ({children}:{children: React.ReactNode}) => {
   return (
-    <Provider store={store}>{children}</Provider>
+    <Provider store={store}>
+        <PersistGate loading= {null} persistor={persistor}>
+        {children}
+        </PersistGate>
+        </Provider>
   )
 }
 
